72c4fa4199d4c353c7cae127cf4096f8799747ac
Weidong Fang <weidong@sinoease.com>
2018-02-27 14:23:42 +1030

    Updated example to use new API

--- example/src/app.js
+++ example/src/app.js
@@ -1,9 +1,4 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-
-import { createSchema, createLoader } from 'archen';
-
-const app = express();
+const Archen = require('./index');
 
 const knex = require('knex')({
   client: 'mysql',
@@ -11,25 +6,13 @@ const knex = require('knex')({
     host: '127.0.0.1',
     user: 'root',
     password: 'secret',
-    database: 'example',
+    database: 'newpro',
   },
   pool: { min: 0, max: 7 },
 });
 
-const schemaFile = require('path').join(__dirname, 'schema.json');
-const schemaData = JSON.parse(require('fs').readFileSync(schemaFile));
+const fileName = require('path').join(__dirname, 'schema.json');
 
-app.use(function(req, res, next) {
-  req.loader = createLoader(knex, schemaData);
-  next();
-});
+const server = new Archen(require('fs').readFileSync(fileName), knex);
 
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema: createSchema(schemaData),
-    graphiql: true,
-  })
-);
-
-app.listen(3000, () => console.log('Example server running on port 3000'));
+server.start(3000, () => console.log('Example server running on port 3000'));
