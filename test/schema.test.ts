3bb4d277567a649c5f527f4340d76afaf50e7373
Weidong Fang <weidong@sinoease.com>
2018-03-22 14:05:10 +1030

    Migrated schema

---
+++ test/schema.test.ts
@@ -0,0 +1,12 @@
+import { createSchema } from '../src/schema';
+import * as helper from './helper';
+import * as graphql from 'graphql';
+import * as fs from 'fs';
+
+const data = helper.getExampleData();
+
+test('create schema', () => {
+  const schema = createSchema(data);
+  //fs.writeFileSync('schema.graphql', graphql.printSchema(schema));
+  expect(schema).not.toBe(undefined);
+});
