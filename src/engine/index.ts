1c00523177677e53495a7bb7841c82696d638261
Weidong Fang <weidong@sinoease.com>
2018-04-22 05:59:45 +0930

    Export getInformationSchema

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,9 +1,10 @@
 import { Connection, Escape, Row } from './connection';
+import { getInformationSchema } from './information_schema';
 
-function createConnection(engine: string, options: any) {
+function createConnection(engine: string = 'mysql', options: any = {}) {
   if (engine === 'mysql') {
     return require('./mysql').default(options);
   }
 }
 
-export { Connection, Escape, Row, createConnection };
+export { Connection, Escape, Row, createConnection, getInformationSchema };
