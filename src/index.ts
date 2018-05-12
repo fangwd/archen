ba7a0ea38adea8d641ddfa499b8719be710e97de
Weidong Fang <weidong@sinoease.com>
2018-05-12 11:45:56 +0930

    Update API

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,59 @@
-import { Accessor } from './accessor';
+import { Accessor, AccessorOptions } from './accessor';
 import { Connection, createConnection } from './engine';
-import { GraphQLSchema } from 'graphql';
-import { Schema } from './model';
-import { SchemaBuilder } from './schema';
+import { Schema, SchemaInfo, SchemaConfig } from './model';
+import { SchemaBuilder, SchemaBuilderOptions } from './schema';
 import { Database } from './database';
+import { getInformationSchema } from './engine';
 
-function createGraphQLSchema(schema: Schema): GraphQLSchema {
-  return new SchemaBuilder(schema).getSchema();
+export interface ArchenConfig {
+  database: {
+    dialect: string;
+    connection: any;
+    schemaInfo: SchemaInfo;
+  };
+  schema?: SchemaConfig;
+  accessor: AccessorOptions;
+  graphql: SchemaBuilderOptions;
 }
 
-export { Schema, Database, Accessor, createGraphQLSchema, createConnection };
+export class Archen {
+  config: ArchenConfig;
+  schema: Schema;
+  graphql: SchemaBuilder;
+
+  constructor(config: ArchenConfig) {
+    this.config = config;
+    if (config.database.schemaInfo) {
+      this.schema = new Schema(config.database.schemaInfo, config.schema);
+      this.graphql = new SchemaBuilder(this.schema, this.config.graphql);
+    }
+  }
+
+  getSchemaInfo(): Promise<SchemaInfo> {
+    const connection = this.createConnection();
+    const name = this.getDatabaseName();
+    return getInformationSchema(connection, name).then(schemaInfo => {
+      this.config.database.schemaInfo = schemaInfo;
+      this.schema = new Schema(schemaInfo, this.config.schema);
+      this.graphql = new SchemaBuilder(this.schema, this.config.graphql);
+      return connection.disconnect().then(() => schemaInfo);
+    });
+  }
+
+  getAccessor(): Accessor {
+    const database = new Database(this.schema, this.createConnection());
+    return new Accessor(database, this.config.accessor);
+  }
+
+  createConnection(): Connection {
+    return createConnection(
+      this.config.database.dialect,
+      this.config.database.connection
+    );
+  }
+
+  getDatabaseName(): string {
+    const connection = this.config.database.connection;
+    return connection.name || connection.database;
+  }
+}
